06b1f479c34a640c93364322e421d283830ceb89
Mountain Cat <tomasz.banas123@gmail.com>
2023-03-08 16:43:00 +0100

    Initial Commit

---
+++ Backend/ApiGateway/ApiGateway/Middleware/ApiFallbackMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Ocelot.Logging;
+using Ocelot.Middleware;
+
+namespace ApiGateway.Middleware;
+
+public class ApiFallbackMiddleware : IMiddleware
+{
+    private async Task HandleRequestAsync(HttpContext httpContext)
+    {
+        await httpContext.Response.WriteAsync("This is a fallback response from Ocelot API Gateway");
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        // Get the incoming request URL
+        var url = context.Request.Path.Value?.ToLower();
+
+        // If URL is somehow null just continue
+        if (url is null)
+        {
+            await next.Invoke(context);
+            return;
+        }
+
+        // Check if the URL does not end with "/api"
+        if (!url.StartsWith("/api"))
+        {
+            // Handle the request directly
+            await HandleRequestAsync(context);
+        }
+        else
+        {
+            // Pass the request on to downstream microservices
+            await next.Invoke(context);
+        }
+    }
+}
---
+++ Backend/ApiGateway/ApiGateway/Program.cs
@@ -0,0 +1,63 @@
+using ApiGateway.Middleware;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// =======================================
+// ===== CONFIGURATION =====
+ 
+var configuration = builder.Configuration;
+
+// Add all json ocelot configuration files 
+string ocelotConfigDirectory = Path.Combine(Directory.GetCurrentDirectory(), "configuration");
+string[] jsonFiles = Directory.GetFiles(ocelotConfigDirectory, "*.json", SearchOption.AllDirectories);
+
+foreach (string jsonFile in jsonFiles)
+{
+    configuration.AddJsonFile(jsonFile, optional: true, reloadOnChange: true);
+}
+
+// Add all JSON files in the current directory and its subdirectories
+configuration.AddJsonFile("*.json", optional: true, reloadOnChange: true);
+
+// =======================================
+// ===== SERVICES =====
+
+var services = builder.Services;
+
+services.AddControllers();
+services.AddOcelot(configuration);
+services.AddSingleton<ApiFallbackMiddleware>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddSwaggerForOcelot(configuration);
+
+// =======================================
+// ===== APP RUN =====
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseRouting();
+
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+app.UseMiddleware<ApiFallbackMiddleware>();
+
+app.UseOcelot().Wait();
+
+app.Run();
