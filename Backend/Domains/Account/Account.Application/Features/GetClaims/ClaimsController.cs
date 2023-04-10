6979aa41662012f15c1ba1243061985417fd0fe8
Mountain Cat <tomasz.banas123@gmail.com>
2023-04-10 15:34:01 +0200

    added get claims endpoint

---
+++ Backend/Domains/Account/Account.Application/Features/GetClaims/ClaimsController.cs
@@ -0,0 +1,29 @@
+﻿using Account.Application.Extensions;
+using LanguageExt.Pipes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
+
+namespace Account.Application.Features.GetClaims;
+
+[ApiController]
+[Route("api/authentication/claims")]
+public class ClaimsController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public ClaimsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetClaims()
+    {
+        var mediatorRequest = new GetClaimsRequest(User);
+
+        var mediatorResult = await _mediator.Send(mediatorRequest);
+
+        return mediatorResult.ToOk();
+    }
+}
---
+++ Backend/Domains/Account/Account.Application/Features/GetClaims/GetClaimsResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+using Account.Application.Dtos.Responses;
+
+namespace Account.Application.Features.GetClaims;
+
+public class GetClaimsResponseDto
+{
+    public required IEnumerable<ClaimDto> Claims { get; set; }
+}
