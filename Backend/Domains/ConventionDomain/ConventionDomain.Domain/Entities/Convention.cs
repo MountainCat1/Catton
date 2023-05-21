2510bee53077d3105b8a92951ee6dbe6e562abcb
Mountain Cat <tomasz.banas123@gmail.com>
2023-05-21 12:01:08 +0200

    finished domain

--- Backend/Domains/ConventionDomain/ConventionDomain.Domain/Entities/Convention.cs
+++ Backend/Domains/ConventionDomain/ConventionDomain.Domain/Entities/Convention.cs
@@ -1,13 +1,55 @@
 ﻿using ConventionDomain.Domain.Abstractions;
+using ConventionDomain.Domain.Validators;
+using FluentValidation;
 
 namespace ConventionDomain.Domain.Entities;
 
+public record ConventionUpdate
+{
+    public string? Name { get; init; }
+    public string? Description { get; init; }  
+    public bool? Active { get; init; }  
+}
+
 public class Convention : Entity
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
 
-    public DateTime CreatedDate { get; set; }
-    public bool Active { get; set; }
+    public DateTime CreatedDate { get;}
+    public bool Active { get; private set; }
+
+
+    private Convention()
+    {
+    }
+
+    public static Convention CreateInstance(string name, string description)
+    {
+        var entity = new Convention
+        {
+            Name = name,
+            Description = description
+        };
+
+        entity.ValidateAndThrow();
+
+        return entity;
+    }
+
+    public void Update(ConventionUpdate update)
+    {
+        Name = update.Name ?? Name;
+        Description = update.Description ?? Description;
+        Active = update.Active ?? Active;
+        
+        ValidateAndThrow();
+    }
+    
+    public void ValidateAndThrow()
+    {
+        new ConventionValidator().ValidateAndThrow(this);
+    }
 }
+
---
+++ Backend/Domains/ConventionDomain/ConventionDomain.Domain/Validators/ConventionValidator.cs
@@ -0,0 +1,13 @@
+﻿using ConventionDomain.Domain.Entities;
+using FluentValidation;
+
+namespace ConventionDomain.Domain.Validators;
+
+public class ConventionValidator : AbstractValidator<Convention>
+{
+    public ConventionValidator()
+    {
+        RuleFor(x => x.Name).NotNull().NotEmpty();
+        RuleFor(x => x.Description).NotNull();
+    }
+}
