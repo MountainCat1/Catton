86c39203cbc04651b492d65232d0f570d093dec1
Mountain Cat <tomasz.banas123@gmail.com>
2023-05-25 18:07:58 +0200

    added sign in view using angular material

--- Frontend/frontend/src/app/app.module.ts
+++ Frontend/frontend/src/app/app.module.ts
@@ -14,6 +14,13 @@ import { PrivateComponent } from './private/private.component';
 import { SecureComponent } from './secure/secure.component';
 import { PanelComponent } from './panel/panel.component';
 import { PublicPopupComponent } from './public-popup/public-popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatChipsModule} from "@angular/material/chips";
 
 @NgModule({
   declarations: [
@@ -31,7 +38,14 @@ import { PublicPopupComponent } from './public-popup/public-popup.component';
     BrowserModule,
     AppRoutingModule,
     GoogleSigninButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule
   ],
   providers: [
     {
