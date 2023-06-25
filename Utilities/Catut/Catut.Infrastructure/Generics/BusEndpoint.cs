60705a2ab411df17df99aa78c8f4909a624c15e4
Mountain Cat <tomasz.banas123@gmail.com>
2023-06-26 00:44:14 +0200

    added mass transit abstractions

---
+++ Utilities/Catut/Catut.Infrastructure/Generics/BusEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Catut.Infrastructure.Abstractions;
+using MassTransit;
+
+namespace Catut.Infrastructure.Generics;
+
+public class BusEndpoint<TMessage> : IBusEndpoint<TMessage> 
+{
+    private readonly IBus _bus;
+    
+    public string QueueName { get; set; }
+
+    public BusEndpoint(IBus bus)
+    {
+        _bus = bus;
+    }
+
+    public async Task SendAsync(TMessage message)
+    {
+        var sendEndpoint = await _bus.GetSendEndpoint(new Uri($"queue:{QueueName}"));
+
+        if (message is null)
+            throw new NullReferenceException("Message was null");
+        
+        await sendEndpoint.Send(message);
+    }
+}
