9be4dac5c733b4a14dba700a8b9306fe0fa9fb76
Mountain Cat <tomasz.banas123@gmail.com>
2023-07-12 22:32:15 +0200

    addded TicketTemplateController.cs

---
+++ Backend/Domains/Payment/Payment.Api/Controller/TicketTemplateController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Payment.Api.Controller;
+
+[ApiController]
+[Route("api/{conventionId:guid}/ticket-templates")]
+public class TicketTemplateController
+{
+}
