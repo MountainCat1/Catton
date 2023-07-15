22d5671e3268dda2d4764c333d108a2df4960f45
Mountain Cat <tomasz.banas123@gmail.com>
2023-07-15 20:30:26 +0200

    removed IOrganizerRepository.cs

--- Backend/Domains/Conventions/Conventions.Domain/Repositories/IOrganizerRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿// using Catut.Domain.Abstractions;
-// using Conventions.Domain.Entities;
-//
-// namespace Conventions.Domain.Repositories;
-//
-// public interface IOrganizerRepository : IRepository<Organizer>
-// {
-// }
