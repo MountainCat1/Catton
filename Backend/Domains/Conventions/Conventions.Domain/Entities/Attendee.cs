777196080735d963777b0a86c791d20bc5214079
Mountain Cat <tomasz.banas123@gmail.com>
2023-08-27 12:23:45 +0200

    added attendee entity

---
+++ Backend/Domains/Conventions/Conventions.Domain/Entities/Attendee.cs
@@ -0,0 +1,51 @@
+﻿using Conventions.Domain.Validators;
+using FluentValidation;
+
+namespace Conventions.Domain.Entities;
+
+public class Attendee
+{
+    public Guid AccountId { private set; get; }
+    
+    public Convention Convention { private set; get; }
+    public string ConventionId { private set; get; }
+    
+    public DateTime CreatedDate { get; set; }
+    
+    // Data from account entity
+    public string AccountUsername { get; set; }
+    public Uri? AccountAvatarUri { get; set; }
+
+
+    private Attendee()
+    {
+        
+    }
+    
+    public static Attendee CreateInstance(
+        Convention convention, 
+        Guid accountId, 
+        string accountUsername,
+        Uri? accountProfilePicture = null,
+        OrganizerRole? role = null)
+    {
+        var entity = new Attendee()
+        {
+            Convention = convention,
+            ConventionId = convention.Id,
+            AccountId = accountId,
+            CreatedDate = DateTime.Now,
+            AccountUsername = accountUsername,
+            AccountAvatarUri = accountProfilePicture
+        };
+
+        entity.ValidateAndThrow();
+
+        return entity;
+    }
+    
+    public void ValidateAndThrow()
+    {
+        new AttendeeOrganizerValidator().ValidateAndThrow(this);
+    }
+}
